71f9223777958314f3383982aec585a6aacb037c
Emiliano Giaquinta <tenlion@hotmail.com>
2019-04-26 09:43:18 +0200

    [FARM-150] My plot detail design basics.

---
+++ lib/ui/myplot/my_plot_detail_screen.dart
@@ -0,0 +1,57 @@
+import 'package:farmsmart_flutter/data/model/crop_entity.dart';
+import 'package:farmsmart_flutter/utils/dimens.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:farmsmart_flutter/redux/app/app_state.dart';
+import 'package:farmsmart_flutter/utils/styles.dart';
+
+import 'myplot_viewmodel.dart';
+
+class CropDetailScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CropDetailState();
+  }
+}
+
+class _CropDetailState extends State<CropDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StoreConnector<AppState, MyPlotViewModel>(
+          builder: (_, viewModel) =>
+              _buildBody(context, viewModel.selectedCrop),
+          converter: (store) => MyPlotViewModel.fromStore(store)),
+    );
+  }
+
+  Widget _buildBody(BuildContext context, CropEntity selectedCropData) {
+    return
+
+      Scaffold(
+          appBar: AppBar(
+            // Define custom appbar here
+            automaticallyImplyLeading: true,
+          ),
+          body : Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: ListView(children: <Widget>[
+                Image.asset('assets/raw/crop_image_mock.png', fit: BoxFit.cover),
+                Padding(
+                    padding: Margins.boxPadding(),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(selectedCropData.summary, style: Styles.titleTextStyle()),
+                        Margins.generalListMargin(),
+                        Html(data: selectedCropData.content),
+                      ],
+                    )),
+              ]))
+      );
+  }
+}
