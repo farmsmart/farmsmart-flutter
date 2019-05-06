b2e90f986c1db10325903a5a067c67680ff247f7
Isaac Martínez <isaac.martinez@basetis.com>
2019-05-06 12:17:47 +0200

    [FARM-162] Navigation to article detail fixed and working

--- lib/redux/middleware/navigation_middleware.dart
+++ lib/redux/middleware/navigation_middleware.dart
@@ -1,6 +1,7 @@
 import 'package:farmsmart_flutter/redux/app/app_state.dart';
 import 'package:farmsmart_flutter/redux/home/myPlot/my_plot_actions.dart';
 import 'package:redux/redux.dart';
+import 'package:farmsmart_flutter/redux/home/discover/discover_actions.dart';
 
 import '../../app_routes.dart';
 import '../keys.dart';
@@ -12,6 +13,9 @@ class NavigationMiddleware extends MiddlewareClass<AppState>{
   void call(Store<AppState> store, dynamic action, NextDispatcher next) {
     if(action is GoToCropDetailAction){
       Keys.navKey.currentState.pushNamed(AppRoutes.cropDetail);
+    }
+    if(action is GoToArticleDetailAction) {
+      Keys.navKey.currentState.pushNamed(AppRoutes.articleDetail);
     }
     next(action);
   }
