28ce4a9ee69e190b8c9c2ba69f8838c2fa5497b7
Jessica Hernandez <jessica.hernandez@basetis.com>
2019-05-06 10:56:23 +0200

    [FARM-68] Fixed article list UI

--- lib/ui/discover/discover_child_item.dart
+++ lib/ui/discover/discover_child_item.dart
@@ -8,72 +8,76 @@ import 'package:farmsmart_flutter/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:farmsmart_flutter/utils/assets.dart';
 
+
 class DiscoveryListItem {
   Widget buildListItem(ArticleEntity articleData, goToArticleDetail) {
     return GestureDetector(
         onTap: () {
-      goToArticleDetail(articleData);
-    },
-    child: Padding(
-    key: ValueKey(articleData.title ?? "No Title"),
-    padding: Margins.boxPadding(),
-    child: Container(
-    height: 80,
-    decoration: BoxDecoration(
-    color: Colors.white,
-    borderRadius: BorderRadius.circular(8.0),
-    boxShadow: BoxShadows.plotListItemShadow()),
-    child: Row(
-    children: <Widget>[
-    ClipRRect(
-    borderRadius: BorderRadius.only(
-    topLeft: Radius.circular(8.0),
-    bottomLeft: Radius.circular(8.0),
-    ),
+          goToArticleDetail(articleData);
+        },
+        child: Padding(
+            key: ValueKey(articleData.title ?? "No Title"),
+            padding: Margins.boxPadding(),
+            child: Container(
+                height: 90,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                    boxShadow: BoxShadows.plotListItemShadow()),
+                child: Row(
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(8.0),
+                        bottomLeft: Radius.circular(8.0),
+                      ),
 //                      child: FadeInImage.assetNetwork(image: cropsData.imagePath, placeholder: 'assets/raw/crop_image_mock.png', fit: BoxFit.cover),
-      child: FadeInImage.assetNetwork(
-          image: articleData.imageUrl,
-          height: 80,
-          width: 80,
-          placeholder: Assets.IMAGE_PLACE_HOLDER,
-          fit: BoxFit.fill),
-    ),
-    Padding(
-    padding: const EdgeInsets.only(left: 10),
-    ),
-    Expanded(
-      flex: 5,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(articleData.title ?? "No Title",
-              style: Styles.subtitleTextStyle()),
-          Margins.generalListSmallMargin(),
-          Text(articleData.summary ?? Strings.myPlotItemDefaultTitle,
-              style: Styles.titleTextStyle()),
-          //Margins.generalListSmallMargin(),
-          //MyPlotItemFooter().build()
-        ],
-      ),
-    ),
-    Expanded(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          Image.asset("assets/icons/profit_loss.png",
-            height: 15,
-            width: 15,
-          )
-          //Margins.generalListSmallMargin(),
-          //MyPlotItemFooter().build()
-        ],
-      ),
-    ),
-    Padding(
-      padding: const EdgeInsets.only(right: 10),
-    ),
-    ],
-    ))));
+                      child: FadeInImage.assetNetwork(
+                          image: articleData.imageUrl,
+                          placeholder: Assets.IMAGE_PLACE_HOLDER,
+                          fit: BoxFit.fill),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                    ),
+                    Expanded(
+                      flex: 5,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(articleData.title ?? "No Title",
+                              style: Styles.subtitleTextStyle()),
+                          Margins.generalListSmallerMargin(),
+                          Text(
+                              articleData.summary ??
+                                  Strings.myPlotItemDefaultTitle,
+                              maxLines: 4,
+                              overflow: TextOverflow.ellipsis,
+                              style: Styles.footerTextStyle()),
+                          //Margins.generalListSmallMargin(),
+                          //MyPlotItemFooter().build()
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Image.asset(
+                            "assets/icons/profit_loss.png",
+                            height: 15,
+                            width: 15,
+                          )
+                          //Margins.generalListSmallMargin(),
+                          //MyPlotItemFooter().build()
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                    ),
+                  ],
+                ))));
   }
 }
