67132525cbf78f8275dd898584ea9d646783227c
Jessica Hernandez <jessica.hernandez@basetis.com>
2019-05-07 16:45:55 +0200

    [FARM-168] Tab navigation test done

--- test/states/home_test.dart
+++ test/states/home_test.dart
@@ -8,8 +8,24 @@ import 'package:farmsmart_flutter/redux/home/screens.dart';
 
 
 void main() {
-  test('home state test', (){
+  test('Navigation test - to my plot', (){
     var state = homeReducer(HomeState.initial(), SwitchTabAction(HomeScreen.MY_PLOT_TAB));
     expect(state, HomeState(loadingStatus: LoadingStatus.LOADING, currentHomeTab: MY_PLOT_TAB));
   });
+
+  test('Navigation test - to discover', (){
+    var state = homeReducer(HomeState.initial(), SwitchTabAction(HomeScreen.ARTICLES_TAB));
+    expect(state, HomeState(loadingStatus: LoadingStatus.LOADING, currentHomeTab: ARTICLES_TAB));
+  });
+
+  test('Navigation test - to commnity', (){
+    var state = homeReducer(HomeState.initial(), SwitchTabAction(HomeScreen.COMMUNITY_TAB));
+    expect(state, HomeState(loadingStatus: LoadingStatus.LOADING, currentHomeTab: COMMUNITY_TAB));
+  });
+
+  test('Navigation test - to profit loss', (){
+    var state = homeReducer(HomeState.initial(), SwitchTabAction(HomeScreen.PROFIT_LOSS_TAB));
+    expect(state, HomeState(loadingStatus: LoadingStatus.LOADING, currentHomeTab: PROFIT_LOSS_TAB));
+  });
+
 }
