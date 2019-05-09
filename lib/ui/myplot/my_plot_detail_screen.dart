e1e26355a683c779580e035b50f5590671386be7
Emiliano Giaquinta <tenlion@hotmail.com>
2019-05-09 11:18:24 +0200

    [MERGE] Update merge conflicts

--- lib/ui/myplot/my_plot_detail_screen.dart
+++ lib/ui/myplot/my_plot_detail_screen.dart
@@ -51,7 +51,7 @@ class _CropDetailState extends State<CropDetailScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Padding(
-                      padding: Margins.boxPadding(),
+                      padding: Paddings.boxPadding(),
                       child: Text(selectedCropData.summary,
                           style: Styles.descriptionTextStyle())),
                   Margins.generalListMargin(),
@@ -59,28 +59,6 @@ class _CropDetailState extends State<CropDetailScreen> {
                   MyPlotDetailProperties().build(cropDetailProperties, context)
                 ],
               ),
-<<<<<<< HEAD
             ])));
-=======
-              child: ListView(children: <Widget>[
-                FadeInImage.assetNetwork(
-                    image: selectedCropData.imageUrl,
-                    height: listImageHeight,
-                    width: listImageWidth,
-                    placeholder: Assets.IMAGE_PLACE_HOLDER,
-                    fit: BoxFit.fitWidth),
-                Padding(
-                    padding: Paddings.boxBigPadding(),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(selectedCropData.summary, style: Styles.descriptionTextStyle()),
-                        Margins.generalListMargin(),
-                        Html(data: selectedCropData.content),
-                      ],
-                    )),
-              ]))
-      );
->>>>>>> [PR Comments] my_plot_child renamed into my_plot_page, discover_child renamed into discover_page, HomeMyPlotChild renamed into HomeMyPlotPage,
   }
 }
