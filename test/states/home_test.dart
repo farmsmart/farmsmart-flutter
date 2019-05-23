d2afb1cc396a5644507e121b6ca05744d327f759
Ross Aubrey-Smith <Ross.Aubrey-Smith@amido.com>
2019-05-23 09:08:06 +0100

    restore test

--- test/states/home_test.dart
+++ test/states/home_test.dart
@@ -6,47 +6,35 @@ import 'package:farmsmart_flutter/redux/home/home_actions.dart';
 import 'package:farmsmart_flutter/model/loading_status.dart';
 import 'package:farmsmart_flutter/redux/home/screens.dart';
 
+
 void main() {
   group('Navigation test', () {
     test('Navigation to my plot', () {
       var state = homeReducer(
           HomeState.initial(), SwitchTabAction(HomeScreen.MY_PLOT_TAB));
-      expect(
-          state,
-          HomeState(
-              loadingStatus: LoadingStatus.LOADING,
-              currentHomeTab: MY_PLOT_TAB));
+      expect(state, HomeState(
+          loadingStatus: LoadingStatus.LOADING, currentHomeTab: MY_PLOT_TAB));
     });
 
     test('Navigation to discover', () {
       var state = homeReducer(
           HomeState.initial(), SwitchTabAction(HomeScreen.ARTICLES_TAB));
-      expect(
-          state,
-          HomeState(
-              loadingStatus: LoadingStatus.LOADING,
-              currentHomeTab: ARTICLES_TAB));
-      expect(true, false);
+      expect(state, HomeState(
+          loadingStatus: LoadingStatus.LOADING, currentHomeTab: ARTICLES_TAB));
     });
 
     test('Navigation to community', () {
       var state = homeReducer(
           HomeState.initial(), SwitchTabAction(HomeScreen.COMMUNITY_TAB));
-      expect(
-          state,
-          HomeState(
-              loadingStatus: LoadingStatus.LOADING,
-              currentHomeTab: COMMUNITY_TAB));
+      expect(state, HomeState(
+          loadingStatus: LoadingStatus.LOADING, currentHomeTab: COMMUNITY_TAB));
     });
 
     test('Navigation to profit loss', () {
       var state = homeReducer(
           HomeState.initial(), SwitchTabAction(HomeScreen.PROFIT_LOSS_TAB));
-      expect(
-          state,
-          HomeState(
-              loadingStatus: LoadingStatus.LOADING,
-              currentHomeTab: PROFIT_LOSS_TAB));
+      expect(state, HomeState(loadingStatus: LoadingStatus.LOADING,
+          currentHomeTab: PROFIT_LOSS_TAB));
     });
   });
 }
