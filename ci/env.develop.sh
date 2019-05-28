cc87bd4327a9a6a93b06735c2de063194fce2a72
Archie <archie@AML0078.local>
2019-05-28 12:36:36 +0100

    Updated workflow to use environment contexts

--- ci/env.develop.sh
+++
@@ -1,3 +0,0 @@
-export TRACK=internal
-export VERSION=${CIRCLE_SHA1}
-export TRACK_URL="https://play.google.com/apps/internaltest/4698261731148956175"
--- ci/env.release.sh
+++
@@ -1,3 +0,0 @@
-export TRACK=alpha
-export VERSION=$(echo $CIRCLE_BRANCH | sed 's/.*\/v\(.*\)/\1/')
-export TRACK_URL="https://play.google.com/apps/testing/co.farmsmart.app"
