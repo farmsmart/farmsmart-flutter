039c3e0585d84da2ac4d638d735e523d3f236b7b
Maribel Dapeton <48677321+maribel4amido@users.noreply.github.com>
2019-06-06 14:58:24 +0100

    Fix stage order (#28)

--- lib/redux/middleware/plot_middleware.dart
+++ lib/redux/middleware/plot_middleware.dart
@@ -1,31 +1,20 @@
 // Middleware in charge of act upon myPlot actions that require network data.
 
 import 'package:farmsmart_flutter/data/model/article_entity.dart';
+import 'package:farmsmart_flutter/data/model/crop_entity.dart';
 import 'package:farmsmart_flutter/data/model/stage_entity.dart';
 import 'package:farmsmart_flutter/data/repositories/articles_directory_repository.dart';
 import 'package:farmsmart_flutter/data/repositories/plot_repository.dart';
-import 'package:farmsmart_flutter/model/loading_status.dart';
 import 'package:farmsmart_flutter/redux/app/app_state.dart';
 import 'package:farmsmart_flutter/redux/home/discover/discover_actions.dart';
 import 'package:farmsmart_flutter/redux/home/myPlot/my_plot_actions.dart';
-import 'package:flutter/foundation.dart';
 import 'package:redux/redux.dart';
 
 class MyPlotMiddleWare extends MiddlewareClass<AppState>{
   @override
   Future call(Store<AppState> store, dynamic action, NextDispatcher next) async {
     if(action is FetchCropListAction) {
-      Stopwatch sw = new Stopwatch();
-      sw.start();
-      var listOfCrops = await PlotRepository.get().getListOfCrops();
-      debugPrint('getListOfCrops() ${sw.elapsed.inMilliseconds} ms ');
-      sw.reset();
-      Future fetchStageFuture = PlotRepository.get().getListOfCropStages(listOfCrops);
-      Future fetchImageFuture = PlotRepository.get().getListOfCropsWithImages(listOfCrops);
-      await Future.wait([fetchStageFuture, fetchImageFuture]);
-      debugPrint('getStagesAndImages() ${sw.elapsed.inMilliseconds} ms ');
-      sw.stop();
-      debugPrint('Fetch crop complete.');
+      List<CropEntity> listOfCrops = await PlotRepository.get().getArticles();
       store.dispatch(UpdateCropListAction(listOfCrops));
     }
 
