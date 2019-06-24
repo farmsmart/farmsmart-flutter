6ddd6e9ea9a48dda828c87fc92656a4705657549
Lee Higgins <lee@wearemobilefirst.com>
2019-06-24 14:39:07 +0200

    Add Simple Article repository interface v1

---
+++ lib/data/repositories/ArticleRepositoryInterface.dart
@@ -0,0 +1,13 @@
+import 'package:farmsmart_flutter/data/model/article_entity.dart';
+
+enum ArticleCollectionGroup {
+  all,
+  featured
+}
+
+abstract class ArticleRepositoryInterface {
+  Future<List<ArticleEntity>> getArticles(ArticleEntityCollection collection);
+  Future<ArticleEntity> getArticle(String uri);
+  Stream<ArticleEntity> observeArticle(String uri);
+  Future<List<ArticleEntity>> get({ArticleCollectionGroup group = ArticleCollectionGroup.all, int limit = 0});
+}
