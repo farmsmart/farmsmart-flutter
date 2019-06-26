c56fc8cab9911384c327495d87eb5befaadc6e16
Jessica Hernandez <jessica.hernandez@basetis.com>
2019-06-26 14:21:23 +0200

    [FARM-355] Bottom sheet resized depending on content

--- lib/ui/common/ActionSheet.dart
+++ lib/ui/common/ActionSheet.dart
@@ -25,11 +25,12 @@ abstract class ActionSheetStyle {
   final Color cornerColor;
   final Color backgroundColor;
   final Color dropLineColor;
+  final Color transparentColor;
 
   final TextStyle mainTextStyle;
   final TextStyle highlightTextStyle;
 
-  final EdgeInsets cardPadding;
+  final EdgeInsets cardEdge;
   final EdgeInsets dropLineEdge;
 
   final Radius borderRadius;
@@ -42,8 +43,8 @@ abstract class ActionSheetStyle {
 
   final int maxLines;
 
-  ActionSheetStyle(this.cornerColor, this.backgroundColor, this.dropLineColor,
-      this.mainTextStyle, this.highlightTextStyle, this.cardPadding,
+  ActionSheetStyle(this.cornerColor, this.backgroundColor, this.dropLineColor, this.transparentColor,
+      this.mainTextStyle, this.highlightTextStyle, this.cardEdge,
       this.dropLineEdge, this.borderRadius, this.dropLineRadius,
       this.dropLineHeight, this.cardHeight, this.iconLineSpace, this.cardElevation, this.maxLines);
 
@@ -57,11 +58,12 @@ class DefaultStyle implements ActionSheetStyle {
   final Color cornerColor = const Color(0xFF737373);
   final Color backgroundColor = const Color(0xFFffffff);
   final Color dropLineColor = const Color(0xFFe0e1ee);
+  final Color transparentColor = const Color(0x00000000);
 
   final TextStyle mainTextStyle = const TextStyle(fontSize: 17, fontWeight: FontWeight.normal, color: titleColor);
   final TextStyle highlightTextStyle = const TextStyle(fontSize: 17, fontWeight: FontWeight.normal, color: highlightColor);
 
-  final EdgeInsets cardPadding = const EdgeInsets.only(left: 32, right: 32);
+  final EdgeInsets cardEdge = const EdgeInsets.only(left: 32, right: 32);
   final EdgeInsets dropLineEdge = const EdgeInsets.only(top: 8);
   
   final Radius borderRadius = const Radius.circular(40);
@@ -81,14 +83,14 @@ class ActionSheet {
   static Widget build(BuildContext context, {ActionSheetStyle actionStyle = const DefaultStyle()}) {
 
     CustomSheetAction recordSale = CustomSheetAction("Record a new Sale", null, false, icon: Icons.clear);
-    CustomSheetAction recordCost = CustomSheetAction("Record a new Cost", null, true, selection:  Icons.style);
-    CustomSheetAction testing = CustomSheetAction("Record a test", null, false);
+    CustomSheetAction recordCost = CustomSheetAction("Record a new Cost", null, false, selection:  Icons.style);
+    CustomSheetAction testing = CustomSheetAction("Record a test", null, true);
     ActionSheetViewModel viewModel = ActionSheetViewModel([recordSale, recordCost, testing], "Cancel");
 
     return Container(
       color: actionStyle.cornerColor, // This line set the transparent background
-      child: Container(
-        padding: actionStyle.dropLineEdge,
+        child: Container(
+          padding: actionStyle.dropLineEdge,
           decoration: BoxDecoration(
               color: actionStyle.backgroundColor,
               borderRadius: BorderRadius.only(
@@ -101,7 +103,7 @@ class ActionSheet {
               shrinkWrap: true,
               itemCount: viewModel.actions.length,
               itemBuilder: (BuildContext context, int index) =>
-            buildActionCell(actionStyle, viewModel.actions[index]),
+            buildActionCell(actionStyle, viewModel.actions[index], viewModel.actions.length, index),
             header: buildDropLine(actionStyle),
             footer: RoundedButton.build(style: LargeRoundedButtonStyle() ,context: context, title: viewModel.buttonTitle)
           )
@@ -119,21 +121,26 @@ class ActionSheet {
     );
   }
 
-  static Widget buildActionCell(ActionSheetStyle actionStyle, CustomSheetAction action) {
-    return Card(
-      elevation: actionStyle.cardElevation,
-      color: Colors.transparent,
-      child: InkWell(
-        onTap: () => print("dsfsdfdsf"),
-        child: Container(
-          padding: actionStyle.cardPadding,
-          alignment: Alignment.center,
-          height: actionStyle.cardHeight,
-          child: Row(
-            children: _buildActionContent(actionStyle, action),
+  static Widget buildActionCell(ActionSheetStyle actionStyle, CustomSheetAction action, int numberOfActions, int currentAction) {
+    return Column(
+      children: <Widget>[
+        Card(
+          elevation: actionStyle.cardElevation,
+          color: actionStyle.transparentColor,
+          child: InkWell(
+            onTap: () => print(action.text),
+            child: Container(
+              padding: actionStyle.cardEdge,
+              alignment: Alignment.center,
+              height: actionStyle.cardHeight,
+              child: Row(
+                children: _buildActionContent(actionStyle, action),
+              ),
+            ),
           ),
         ),
-      ),
+        currentAction == numberOfActions-1 ?  Row() : ListDivider.build(),
+      ],
     );
   }
 
@@ -141,14 +148,14 @@ class ActionSheet {
     List<Widget> listBuilder = [];
     if (action.icon != null) {
       listBuilder.add(Icon(action.icon));
-      listBuilder.add(SizedBox(width: 21.5));
-      listBuilder.add(Text(action.text, style: action.isHighlighted ? actionStyle.mainTextStyle : actionStyle.highlightTextStyle));
+      listBuilder.add(SizedBox(width: actionStyle.iconLineSpace));
+      listBuilder.add(Text(action.text, style: action.isHighlighted ? actionStyle.highlightTextStyle : actionStyle.mainTextStyle));
     } else if (action.selection != null) {
-      listBuilder.add(Text(action.text, style: action.isHighlighted ? actionStyle.mainTextStyle : actionStyle.highlightTextStyle));
-      listBuilder.add(SizedBox(width: 21.5));
+      listBuilder.add(Text(action.text, style: action.isHighlighted ? actionStyle.highlightTextStyle : actionStyle.mainTextStyle));
+      listBuilder.add(SizedBox(width: actionStyle.iconLineSpace));
       listBuilder.add(Icon(action.selection));
     } else {
-      listBuilder.add(Text(action.text, style: action.isHighlighted ? actionStyle.mainTextStyle : actionStyle.highlightTextStyle));
+      listBuilder.add(Text(action.text, style: action.isHighlighted ? actionStyle.highlightTextStyle : actionStyle.mainTextStyle));
     }
     return listBuilder;
   }
