7f406cab2d04ed11537fcfe82bb355826c793d60
Jessica Hernandez <jessica.hernandez@basetis.com>
2019-07-02 16:59:49 +0200

    [FARM-355] Fixed bottom action sheet

--- lib/ui/common/PlaygroundWidgetList.dart
+++ lib/ui/common/PlaygroundWidgetList.dart
@@ -10,9 +10,9 @@ class PlaygroundWidgetList {
     ActionSheetListItem(style: ActionSheetListItemStyle.defaultStyle(), viewModel: MockActionSheetViewModel.buildStandard().actions.first, numberOfActions: 1, currentAction: 1),
     ActionSheetListItem(style: ActionSheetListItemStyle.defaultStyle(), viewModel: MockActionSheetViewModel.buildWithIcon().actions.first, numberOfActions: 1, currentAction: 1),
     ActionSheetListItem(style: ActionSheetListItemStyle.selectableStyle(), viewModel: MockActionSheetViewModel.buildWithCheckBox().actions.first, numberOfActions: 1, currentAction: 1),
-    ActionSheet(viewModel: MockActionSheetViewModel.buildStandard(), style: ActionSheetStyle.defaultStyle()),
-    ActionSheet(viewModel: MockActionSheetViewModel.buildStandardBigger(), style: ActionSheetStyle.defaultStyle()),
-    ActionSheet(viewModel: MockActionSheetViewModel.buildWithIcon(), style: ActionSheetStyle.defaultStyle()),
+    ActionSheet(viewModel: MockActionSheetViewModel.buildStandard(), style: ActionSheetStyle.defaultStyle(), cellStyle: ActionSheetListItemStyle.defaultStyle()),
+    ActionSheet(viewModel: MockActionSheetViewModel.buildStandardBigger(), style: ActionSheetStyle.defaultStyle(), cellStyle: ActionSheetListItemStyle.defaultStyle()),
+    ActionSheet(viewModel: MockActionSheetViewModel.buildWithIcon(), style: ActionSheetStyle.defaultStyle(), cellStyle: ActionSheetListItemStyle.defaultStyle()),
     ActionSheet(viewModel: MockActionSheetViewModel.buildWithCheckBox(), style: ActionSheetStyle.defaultStyle(), cellStyle: ActionSheetListItemStyle.selectableStyle())
   ];
 }
