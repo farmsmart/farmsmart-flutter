e2f9459743584988ee07af3b143e162ba4da0701
Isaac Martínez <isaac.martinez@basetis.com>
2019-07-08 14:43:10 +0200

    [FARM-279]-Reformat code

--- lib/ui/common/CircularProgress.dart
+++ lib/ui/common/CircularProgress.dart
@@ -24,46 +24,65 @@ abstract class CircularProgressStyle {
       this.lineColor, this.testWidth, this.edgePadding);
 
   CircularProgressStyle copyWith(
-  {double height, double width, Color completeColor, Color lineColor,
-    double testWidth, EdgeInsets edgePadding});
+      {double height,
+      double width,
+      Color completeColor,
+      Color lineColor,
+      double testWidth,
+      EdgeInsets edgePadding});
 }
 
 class _DefaultStyle implements CircularProgressStyle {
-  final double height =  87;
-  final double width =  87;
+  final double height = 87;
+  final double width = 87;
   final Color completeColor = const Color(0xff24d900);
-  final Color lineColor =  Colors.transparent;
+  final Color lineColor = Colors.transparent;
   final double testWidth = 3;
   final EdgeInsets edgePadding = const EdgeInsets.all(4.0);
 
-  const _DefaultStyle({double height, double width, Color completeColor, Color lineColor,
-  double testWidth, EdgeInsets edgePadding});
+  const _DefaultStyle(
+      {double height,
+      double width,
+      Color completeColor,
+      Color lineColor,
+      double testWidth,
+      EdgeInsets edgePadding});
 
   @override
-  CircularProgressStyle copyWith({double height, double width, Color completeColor,
-    Color lineColor, double testWidth, EdgeInsets edgePadding}) {
+  CircularProgressStyle copyWith(
+      {double height,
+      double width,
+      Color completeColor,
+      Color lineColor,
+      double testWidth,
+      EdgeInsets edgePadding}) {
     return _DefaultStyle(
-      height: height ?? this.height,
-      width: width ?? this.width,
-      completeColor: completeColor ?? this.completeColor,
-      lineColor: lineColor ?? this.lineColor,
-      testWidth: testWidth ?? this.testWidth,
-      edgePadding: edgePadding ?? this.edgePadding
-    );
+        height: height ?? this.height,
+        width: width ?? this.width,
+        completeColor: completeColor ?? this.completeColor,
+        lineColor: lineColor ?? this.lineColor,
+        testWidth: testWidth ?? this.testWidth,
+        edgePadding: edgePadding ?? this.edgePadding);
   }
 }
 
 const CircularProgressStyle _defaultStyle = const _DefaultStyle();
 
-
 class CircularProgress extends StatefulWidget {
   final CircularProgressStyle _style;
   final CircularProgressViewModel _viewModel;
 
-  CircularProgress({Key key, CircularProgressViewModel viewModel, CircularProgressStyle style = _defaultStyle})
-      : this._viewModel = viewModel, this._style = style, super(key: key);
+  CircularProgress(
+      {Key key,
+      CircularProgressViewModel viewModel,
+      CircularProgressStyle style = _defaultStyle})
+      : this._viewModel = viewModel,
+        this._style = style,
+        super(key: key);
+
   @override
-  _CircularProgressState createState() => _CircularProgressState(_viewModel, _style);
+  _CircularProgressState createState() =>
+      _CircularProgressState(_viewModel, _style);
 }
 
 class _CircularProgressState extends State<CircularProgress> {
@@ -98,9 +117,7 @@ class _CircularProgressState extends State<CircularProgress> {
           child: Padding(
             padding: _style.edgePadding,
             child: ClipOval(
-              child: Stack(
-                children: _buildPlotContent()
-              ),
+              child: Stack(children: _buildPlotContent()),
             ),
           ),
         ),
@@ -110,24 +127,20 @@ class _CircularProgressState extends State<CircularProgress> {
 
   List<Widget> _buildPlotContent({Future<String> imageUrl}) {
     List<Widget> listBuilder = [];
-    if (imageUrl != null){
+    if (imageUrl != null) {
       listBuilder.add(
-          NetworkImageFromFuture(imageUrl,
-              height: 80,
-              width: 80,
-              fit: BoxFit.cover),
+        NetworkImageFromFuture(imageUrl,
+            height: 80, width: 80, fit: BoxFit.cover),
       );
-      listBuilder.add(
-          Positioned.fill(
-              child: Container(
-                color: Color(0x1425df0c),
-              ))
-      );
+      listBuilder.add(Positioned.fill(
+          child: Container(
+        color: Color(0x1425df0c),
+      )));
     }
     return listBuilder;
   }
 
-  /*
+/*
   void startTimer() {
     Timer.periodic(Duration(milliseconds: 500), (timer) {
       if(this.mounted){
