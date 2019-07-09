95b3f61c1229a71d1143a90bcd232fed1b8541c7
Jessica Hernandez <jessica.hernandez@basetis.com>
2019-07-09 15:52:07 +0200

    [FARM-428] Dogtag color and paddings addapted

--- lib/ui/common/Dogtag.dart
+++ lib/ui/common/Dogtag.dart
@@ -35,11 +35,11 @@ class DogTagStyle {
     return DogTagStyle(
         backgroundColor: Color(0x1425df0c),
         borderRadius: BorderRadius.all(Radius.circular(20.0)),
-        edgePadding: EdgeInsets.only(top: 8.5, right: 12, left: 12, bottom: 8),
+        edgePadding: EdgeInsets.only(top: 4.5, right: 12, left: 12, bottom: 4.5),
         titleTextStyle: TextStyle(
             fontSize: 15,
-            fontWeight: FontWeight.normal,
-            color: Color(0xff25df0c)),
+            fontWeight: FontWeight.w500,
+            color: Color(0xff21c400)),
         maxLines: 1,
         iconSize: 8,
         spacing: 5.5);
@@ -50,7 +50,7 @@ class DogTagStyle {
         backgroundColor: Color(0x14ff8d4f),
         titleTextStyle: TextStyle(
             fontSize: 15,
-            fontWeight: FontWeight.normal,
+            fontWeight: FontWeight.w500,
             color: Color(0xffff8d4f)));
   }
 
@@ -60,8 +60,8 @@ class DogTagStyle {
             EdgeInsets.only(left: 12, top: 5.5, right: 12, bottom: 5.5),
         titleTextStyle: TextStyle(
             fontSize: 11,
-            fontWeight: FontWeight.normal,
-            color: Color(0xff25df0c)));
+            fontWeight: FontWeight.w500,
+            color: Color(0xff21c400)));
   }
 
   DogTagStyle copyWith(
