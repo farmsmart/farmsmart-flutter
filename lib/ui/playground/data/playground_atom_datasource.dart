a6b0c630f545d92d0dfd393a067da801f2a65a11
Isaac Martínez <isaac.martinez@basetis.com>
2019-07-10 11:11:58 +0200

    [FARM-428]PR comment, Added "comas" and reformat code

--- lib/ui/playground/data/playground_atom_datasource.dart
+++ lib/ui/playground/data/playground_atom_datasource.dart
@@ -20,110 +20,129 @@ class PlayGroundAtomDataSource implements PlaygroundDataSource {
     return [
       //Add your atoms
       PlaygroundWidget(
-          title: 'RoundedButton Large',
-          child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(20.0),
-            height: 96,
-            child: RoundedButton(
-                viewModel: MockRoundedButtonViewModel.buildLarge(),
-                style: RoundedButtonStyle.largeRoundedButtonStyle()),
-          )),
+        title: 'RoundedButton Large',
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(20.0),
+          height: 96,
+          child: RoundedButton(
+            viewModel: MockRoundedButtonViewModel.buildLarge(),
+            style: RoundedButtonStyle.largeRoundedButtonStyle(),
+          ),
+        ),
+      ),
 
       PlaygroundWidget(
-          title: 'RoundedButton Compact',
-          child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(20.0),
-            height: 64,
-            child: RoundedButton(
-                viewModel: MockRoundedButtonViewModel.buildCompact(),
-                style: RoundedButtonStyle.defaultStyle()),
-          )),
+        title: 'RoundedButton Compact',
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(20.0),
+          height: 64,
+          child: RoundedButton(
+            viewModel: MockRoundedButtonViewModel.buildCompact(),
+            style: RoundedButtonStyle.defaultStyle(),
+          ),
+        ),
+      ),
 
       PlaygroundWidget(
-          title: 'RoundedButton Compact Big',
-          child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(20.0),
-            height: 88,
-            child: RoundedButton(
-                viewModel: MockRoundedButtonViewModel.buildCompact(),
-                style: RoundedButtonStyle.bigRoundedButton()),
-          )),
+        title: 'RoundedButton Compact Big',
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(20.0),
+          height: 88,
+          child: RoundedButton(
+            viewModel: MockRoundedButtonViewModel.buildCompact(),
+            style: RoundedButtonStyle.bigRoundedButton(),
+          ),
+        ),
+      ),
 
       PlaygroundWidget(
-          title: 'DogTag Days',
-          child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(20.0),
-            height: 100,
-            child: DogTag(
-                viewModel: MockDogTagViewModel.buildWithText(),
-                style: DogTagStyle.compactStyle()),
-          )),
+        title: 'DogTag Days',
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(20.0),
+          height: 100,
+          child: DogTag(
+            viewModel: MockDogTagViewModel.buildWithText(),
+            style: DogTagStyle.compactStyle(),
+          ),
+        ),
+      ),
 
       PlaygroundWidget(
-          title: 'DogTag Positive',
-          child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(20.0),
-            height: 1000,
-            child: DogTag(
-                viewModel: MockDogTagViewModel.buildWithPositiveNumber(),
-                style: DogTagStyle.defaultStyle()),
-          )),
+        title: 'DogTag Positive',
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(20.0),
+          height: 1000,
+          child: DogTag(
+            viewModel: MockDogTagViewModel.buildWithPositiveNumber(),
+            style: DogTagStyle.defaultStyle(),
+          ),
+        ),
+      ),
 
       PlaygroundWidget(
-          title: 'DogTag Negative',
-          child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(20.0),
-            height: 100,
-            child: DogTag(
-                viewModel: MockDogTagViewModel.buildWithNegativeNumber(),
-                style: DogTagStyle.negativeStyle()),
-          )),
+        title: 'DogTag Negative',
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(20.0),
+          height: 100,
+          child: DogTag(
+            viewModel: MockDogTagViewModel.buildWithNegativeNumber(),
+            style: DogTagStyle.negativeStyle(),
+          ),
+        ),
+      ),
 
       PlaygroundWidget(
-          title: 'Simple Action Sheet Item',
-          child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.symmetric(vertical: 20.0),
-            child: ActionSheetListItem(
-                viewModel:
-                    MockActionSheetViewModel.buildStandard().actions.first),
-          )),
+        title: 'Simple Action Sheet Item',
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(vertical: 20.0),
+          child: ActionSheetListItem(
+              viewModel:
+                  MockActionSheetViewModel.buildStandard().actions.first),
+        ),
+      ),
       PlaygroundWidget(
-          title: 'Action Sheet Item with Icon',
-          child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.symmetric(vertical: 20.0),
-            child: ActionSheetListItem(
-                viewModel:
-                    MockActionSheetViewModel.buildWithIcon().actions.first),
-          )),
+        title: 'Action Sheet Item with Icon',
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(vertical: 20.0),
+          child: ActionSheetListItem(
+              viewModel:
+                  MockActionSheetViewModel.buildWithIcon().actions.first),
+        ),
+      ),
       PlaygroundWidget(
-          title: 'Action Sheet Item with Checkbox',
-          child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.symmetric(vertical: 20.0),
-            child: ActionSheetListItem(
-                viewModel:
-                    MockActionSheetViewModel.buildWithCheckBox().actions.first),
-          )),
+        title: 'Action Sheet Item with Checkbox',
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(vertical: 20.0),
+          child: ActionSheetListItem(
+              viewModel:
+                  MockActionSheetViewModel.buildWithCheckBox().actions.first),
+        ),
+      ),
       StandardListItem(
-          viewModel: ArticleListItemViewModelTransformer(
-                  detailTransformer: ArticleDetailViewModelTransformer(
-                      listItemTransformer:
-                          ArticleListItemViewModelTransformer()))
-              .transform(from: MockArticle.build())),
+        viewModel: ArticleListItemViewModelTransformer(
+                detailTransformer: ArticleDetailViewModelTransformer(
+                    listItemTransformer: ArticleListItemViewModelTransformer()))
+            .transform(
+          from: MockArticle.build(),
+        ),
+      ),
       HeroListItem(
-          viewModel: ArticleListItemViewModelTransformer(
-                  detailTransformer: ArticleDetailViewModelTransformer(
-                      listItemTransformer:
-                          ArticleListItemViewModelTransformer()))
-              .transform(from: MockArticle.build())),
+        viewModel: ArticleListItemViewModelTransformer(
+                detailTransformer: ArticleDetailViewModelTransformer(
+                    listItemTransformer: ArticleListItemViewModelTransformer()))
+            .transform(
+          from: MockArticle.build(),
+        ),
+      ),
     ];
   }
 }
