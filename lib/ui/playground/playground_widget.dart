c86ff5ccdf4cb0b6b720da381463e15d79cdefa1
David Franquet <david@wearemobilefirst.com>
2019-07-11 08:41:08 +0200

    [FARM-278] Make Playgroundwidget stateless again

--- lib/ui/playground/playground_widget.dart
+++ lib/ui/playground/playground_widget.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/widgets.dart';
 
-class PlaygroundWidget extends StatefulWidget {
+class PlaygroundWidget extends StatelessWidget {
   final String title;
   final Widget child;
 
   PlaygroundWidget({Key key, this.title, this.child});
 
   @override
-  _PlaygroundWidgetState createState() => _PlaygroundWidgetState();
-}
-
-class _PlaygroundWidgetState extends State<PlaygroundWidget> {
-  @override
   Widget build(BuildContext context) {
-    return widget.child;
+    return child;
   }
 }
-
