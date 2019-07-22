006bdc8a7f64227a0123d36e6c28fbf85e142a16
Isaac Martínez <isaac.martinez@basetis.com>
2019-07-22 11:33:06 +0200

    [FARM-432]Added style to the General Alert.

--- lib/ui/common/AlertWidget.dart
+++ lib/ui/common/AlertWidget.dart
@@ -1,4 +1,3 @@
-
 import 'package:farmsmart_flutter/ui/common/roundedButton.dart';
 import 'package:farmsmart_flutter/ui/mockData/MockRoundedButtonViewModel.dart';
 import 'package:flutter/material.dart';
@@ -14,16 +13,57 @@ class AlertWidget extends StatelessWidget {
 
   Future<bool> _confirmDialog(BuildContext context) {
     return showDialog<bool>(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Are u sure"),
-          content: RoundedButton(viewModel: MockRoundedButtonViewModel.buildLarge(),
-            style: RoundedButtonStyle.largeRoundedButtonStyle(),
-          ),
-        );
-      }
-    );
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              title: Text("Begin Stage 2"),
+              titleTextStyle: TextStyle(
+                  color: Color(0xff1a1b46),
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    "Are you sure you want to begin Stage 2 - Planting?",
+                    style: TextStyle(
+                        color: Color(0xff1a1b46),
+                        fontSize: 17,
+                        fontWeight: FontWeight.normal),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: _showBla(),
+                  )
+                ],
+              ),);
+        });
+  }
+
+  List<Widget> _showBla() {
+    List<Widget> listBuilder = [
+      Expanded(
+        child: RoundedButton(
+          viewModel: RoundedButtonViewModel(title: "Cancel", onTap: () {}),
+          style: RoundedButtonStyle.actionSheetLargeRoundedButton().copyWith(height: 48, width: 112),
+        ),
+      )
+    ];
+    listBuilder.add(SizedBox(
+      width: 12,
+    ));
+    listBuilder.add(Expanded(
+      child: RoundedButton(
+        viewModel: RoundedButtonViewModel(title: "Yes", onTap: () {}),
+        style: RoundedButtonStyle.actionSheetLargeRoundedButton()
+            .copyWith(height: 48, width: 112, backgroundColor: Color(0xff24d900), buttonTextStyle: TextStyle(color: Color(0xffffffff))),
+      ),
+    ));
+    return listBuilder;
   }
 }
