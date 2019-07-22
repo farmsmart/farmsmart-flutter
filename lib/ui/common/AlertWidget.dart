8a7f54ec8e2b0c15f5cdabd290647e429f8c913c
Isaac Martínez <isaac.martinez@basetis.com>
2019-07-22 12:59:23 +0200

    [FARM-432]Custom Alert added

--- lib/ui/common/AlertWidget.dart
+++ lib/ui/common/AlertWidget.dart
@@ -13,36 +13,51 @@ class AlertWidget extends StatelessWidget {
 
   Future<bool> _confirmDialog(BuildContext context) {
     return showDialog<bool>(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return AlertDialog(
-              title: Text("Begin Stage 2"),
-              titleTextStyle: TextStyle(
-                  color: Color(0xff1a1b46),
-                  fontSize: 27,
-                  fontWeight: FontWeight.bold),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    "Are you sure you want to begin Stage 2 - Planting?",
-                    style: TextStyle(
-                        color: Color(0xff1a1b46),
-                        fontSize: 17,
-                        fontWeight: FontWeight.normal),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    children: _showBla(),
-                  )
-                ],
-              ),);
-        });
+      context: context,
+      barrierDismissible: false,
+      builder: (_) => Center(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 24),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  color: Colors.white,
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 35),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "Begin Stage 2",
+                      style: TextStyle(
+                          color: Color(0xff1a1b46),
+                          fontSize: 27,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          "Are you sure you want to begin Stage 2 - Planting?",
+                          style: TextStyle(
+                              color: Color(0xff1a1b46),
+                              fontSize: 17,
+                              fontWeight: FontWeight.normal),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Row(
+                          children: _showBla(),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+    );
   }
 
   List<Widget> _showBla() {
@@ -50,7 +65,8 @@ class AlertWidget extends StatelessWidget {
       Expanded(
         child: RoundedButton(
           viewModel: RoundedButtonViewModel(title: "Cancel", onTap: () {}),
-          style: RoundedButtonStyle.actionSheetLargeRoundedButton().copyWith(height: 48, width: 112),
+          style: RoundedButtonStyle.actionSheetLargeRoundedButton()
+              .copyWith(height: 48, width: 112),
         ),
       )
     ];
@@ -60,8 +76,11 @@ class AlertWidget extends StatelessWidget {
     listBuilder.add(Expanded(
       child: RoundedButton(
         viewModel: RoundedButtonViewModel(title: "Yes", onTap: () {}),
-        style: RoundedButtonStyle.actionSheetLargeRoundedButton()
-            .copyWith(height: 48, width: 112, backgroundColor: Color(0xff24d900), buttonTextStyle: TextStyle(color: Color(0xffffffff))),
+        style: RoundedButtonStyle.actionSheetLargeRoundedButton().copyWith(
+            height: 48,
+            width: 112,
+            backgroundColor: Color(0xff24d900),
+            buttonTextStyle: TextStyle(color: Color(0xffffffff), fontSize: 10),),
       ),
     ));
     return listBuilder;
