14b898fbf77e33d4e2e6c053d9c7bad601f9f022
Isaac Martínez <isaac.martinez@basetis.com>
2019-07-22 15:18:35 +0200

    [FARM-432]Measures checked for match the design

--- lib/ui/common/AlertWidget.dart
+++ lib/ui/common/AlertWidget.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 class AlertWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FlatButton(
-      child: Text("TEST"),
+    return RaisedButton(
+      child: Text("Press Me"),
       onPressed: () => _confirmDialog(context),
     );
   }
@@ -15,45 +15,52 @@ class AlertWidget extends StatelessWidget {
     return showDialog<bool>(
       context: context,
       barrierDismissible: false,
-      builder: (_) => Center(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 24),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+      builder: (_) => Material(
+            type: MaterialType.transparency,
+            child: Center(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 24),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(24.0)),
                   color: Colors.white,
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 35),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                      "Begin Stage 2",
-                      style: TextStyle(
-                          color: Color(0xff1a1b46),
-                          fontSize: 27,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 26,),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text(
-                          "Are you sure you want to begin Stage 2 - Planting?",
-                          style: TextStyle(
-                              color: Color(0xff1a1b46),
-                              fontSize: 17,
-                              fontWeight: FontWeight.normal),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Row(
-                          children: _showBla(),
-                        )
-                      ],
-                    )
-                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(left: 32, right: 32, bottom: 31, top: 31),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        "Begin Stage 2",
+                        style: TextStyle(
+                            color: Color(0xff1a1b46),
+                            fontSize: 27,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 19,
+                      ),
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            "Are you sure you want to beging Stage 2 – Planting?",
+                            style: TextStyle(
+                                color: Color(0xff1a1b46),
+                                fontSize: 17,
+                                height: 1.1,
+                                fontWeight: FontWeight.normal),
+                          ),
+                          SizedBox(
+                            height: 28,
+                          ),
+                          Row(
+                            children: _showBla(),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -67,21 +74,22 @@ class AlertWidget extends StatelessWidget {
         child: RoundedButton(
           viewModel: RoundedButtonViewModel(title: "Cancel", onTap: () {}),
           style: RoundedButtonStyle.actionSheetLargeRoundedButton()
-              .copyWith(height: 48, width: 112),
+              .copyWith(height: 48, width: 120, borderRadius: BorderRadius.all(Radius.circular(14))),
         ),
       )
     ];
     listBuilder.add(SizedBox(
-      width: 12,
+      width: 8,
     ));
     listBuilder.add(Expanded(
       child: RoundedButton(
         viewModel: RoundedButtonViewModel(title: "Yes", onTap: () {}),
         style: RoundedButtonStyle.actionSheetLargeRoundedButton().copyWith(
             height: 48,
-            width: 112,
+            width: 120,
             backgroundColor: Color(0xff24d900),
-            buttonTextStyle: TextStyle(color: Color(0xffffffff), fontSize: 10),),
+            buttonTextStyle: TextStyle(color: Color(0xffffffff), fontSize: 15),
+            borderRadius: BorderRadius.all(Radius.circular(14))),
       ),
     ));
     return listBuilder;
