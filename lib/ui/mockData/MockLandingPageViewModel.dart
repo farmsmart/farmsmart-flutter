d29fd8d0004e59fcc56c9f9d8124488baaf4c8af
Isaac Martínez <isaac.martinez@basetis.com>
2019-07-22 11:36:56 +0200

    [FARM-48]PR comments

--- lib/ui/mockData/MockLandingPageViewModel.dart
+++ lib/ui/mockData/MockLandingPageViewModel.dart
@@ -2,18 +2,18 @@ import 'package:farmsmart_flutter/ui/LandingPage.dart';
 import 'package:intl/intl.dart';
 
 class _Strings {
-  static String detailText = ("A network and knowledge source for farmers in Kenya");
-  static String actionText = ("Get Started");
-  static String footerText = ("Switch Langauge – Badilisha Lugha");
+  static String detailText = "A network and knowledge source for farmers in Kenya";
+  static String actionText = "Get Started";
+  static String footerText = "Switch Langauge – Badilisha Lugha";
 }
 
 
 class MockLandingPageViewModel {
   static LandingPageViewModel build() {
     return LandingPageViewModel(
-        detailText: _Strings.detailText,
-        actionText: _Strings.actionText,
-        footerText: _Strings.footerText,
+        detailText: Intl.message(_Strings.detailText),
+        actionText: Intl.message(_Strings.actionText),
+        footerText: Intl.message(_Strings.footerText),
         headerImage: "assets/raw/illustration_welcome.png",
         subtitleImage: "assets/raw/logo_default.png");
   }
