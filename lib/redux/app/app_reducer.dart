32a50dc853d73a78c740c8cc7f15db5ba2817e35
Jessica Hernandez <jessica.hernandez@basetis.com>
2019-07-30 10:36:50 +0200

    [FARM-63] Revert app_reducer

--- lib/redux/app/app_reducer.dart
+++ lib/redux/app/app_reducer.dart
@@ -1,6 +1,7 @@
 import 'package:farmsmart_flutter/redux/app/app_state.dart';
 import 'package:farmsmart_flutter/redux/home/home_reducer.dart';
 
-AppState appReducer(AppState state, dynamic action) => AppState(
-      homeState: homeReducer(state.homeState, action),
+AppState appReducer(AppState state, dynamic action) =>
+    AppState(
+      homeState: homeReducer(state.homeState ,action),
     );
