c99bedb04910a56d789d9b6bb49f016a811bdecb
Lee Higgins <lee@wearemobilefirst.com>
2019-08-11 15:18:45 +0200

    Disabled getting alternate images for now as it does not work for all images.

--- lib/model/repositories/image/implementation/ImageRepositoryFlamelink.dart
+++ lib/model/repositories/image/implementation/ImageRepositoryFlamelink.dart
@@ -7,97 +7,104 @@ import 'package:farmsmart_flutter/model/model/ImageURLProvider.dart';
 import '../../FlameLink.dart';
 import '../ImageRepositoryInterface.dart';
 
-
 class ImageEntityFields {
-    static String sizes = "sizes";
-    static String width = "width";
-    static String height = "height";
-    static String path = "path";
-    static String file = "file";
+  static String sizes = "sizes";
+  static String width = "width";
+  static String height = "height";
+  static String path = "path";
+  static String file = "file";
 }
 
 class _Strings {
-  static const sizesFolder  = "sized";
+  static const sizesFolder = "sized";
   static const pathDivider = "/";
 }
 
 class FlamelinkImageEntity extends ImageEntity {
   final FlameLink _cms;
   final List<ImageEntity> otherSizes;
-  FlamelinkImageEntity(FlameLink cms, int width, int height, String path, List<ImageEntity> otherSizes) : this._cms = cms, this.otherSizes = otherSizes, super(width, height, path, null) {
+  FlamelinkImageEntity(FlameLink cms, int width, int height, String path,
+      List<ImageEntity> otherSizes)
+      : this._cms = cms,
+        this.otherSizes = otherSizes,
+        super(width, height, path, null) {
     this.urlProvider = FlameLinkImageProvider(_cms, this);
   }
-} 
+}
 
 ImageEntity _transform(FlameLink cms, DocumentSnapshot snapshot) {
-    final imageFileNamePath = snapshot.data[ImageEntityFields.file];
-    
-    final alternateSizesObjs = snapshot.data[ImageEntityFields.sizes].map((imageSize) {
-      final path = _Strings.sizesFolder + _Strings.pathDivider +  imageSize[ImageEntityFields.path] + _Strings.pathDivider + imageFileNamePath;
-      final width = imageSize[ImageEntityFields.width];
-      final height = imageSize[ImageEntityFields.height];
-      return FlamelinkImageEntity(cms, width, height, path, []);
-    }).toList();
-    final alternateSizes = List<ImageEntity>.from(alternateSizesObjs);
-    return FlamelinkImageEntity(cms ,0, 0, imageFileNamePath, alternateSizes);
+  final imageFileNamePath = snapshot.data[ImageEntityFields.file];
+
+  final alternateSizesObjs =
+      snapshot.data[ImageEntityFields.sizes].map((imageSize) {
+    final path = _Strings.sizesFolder +
+        _Strings.pathDivider +
+        imageSize[ImageEntityFields.path] +
+        _Strings.pathDivider +
+        imageFileNamePath;
+    final width = imageSize[ImageEntityFields.width];
+    final height = imageSize[ImageEntityFields.height];
+    return FlamelinkImageEntity(cms, width, height, path, []);
+  }).toList();
+  final alternateSizes = List<ImageEntity>.from(alternateSizesObjs);
+  return FlamelinkImageEntity(cms, 0, 0, imageFileNamePath, alternateSizes);
 }
 
 class FlameLinkImageProvider implements ImageURLProvider {
   final FlameLink _cms;
   final FlamelinkImageEntity _entity;
-  FlameLinkImageProvider(FlameLink cms, FlamelinkImageEntity entity) : _cms = cms, _entity = entity;
+  FlameLinkImageProvider(FlameLink cms, FlamelinkImageEntity entity)
+      : _cms = cms,
+        _entity = entity;
 
   @override
   Future<String> urlToFit({double width, double height}) {
-    var storageReference = _cms.images(path: _entity.path); 
-    if (width != null){
+    final originalImage = _cms.images(path: _entity.path);
+    /*if (width != null) {
       final targetWidth = width.toInt();
       var alternateImages = _entity.otherSizes;
-      alternateImages.sort((a,b){
+      alternateImages.sort((a, b) {
         return a.width.compareTo(b.width);
       });
-    
+
       for (var image in alternateImages) {
-        if((image.width > targetWidth) ) {
-          storageReference = _cms.images(path: image.path); 
-          break;
+        if ((image.width > targetWidth)) {
+          return _cms
+              .images(path: image.path)
+              .getDownloadURL()
+              .then((value) => value.toString());
         }
       }
-    }
-    
-    return storageReference.getDownloadURL().then((value){
-      return value;
-    }, onError: (error){
-      print(error);
-    });
+    }*/
+    return originalImage.getDownloadURL().then((value) => value.toString());
   }
-  
 }
 
 class ImageEntityCollectionFlamelink implements EntityCollection<ImageEntity> {
   final FlamelinkDocumentCollection _collection;
 
   ImageEntityCollectionFlamelink({FlamelinkDocumentCollection collection})
-      :_collection = collection;
+      : _collection = collection;
 
   @override
   Future<List<ImageEntity>> getEntities({int limit = 0}) {
-    final imageFutures = _collection.getDocuments().then((documents) { 
-      return documents.map((document) => _transform(_collection.cms, document)).toList(); 
-      });
+    final imageFutures = _collection.getDocuments().then((documents) {
+      return documents
+          .map((document) => _transform(_collection.cms, document))
+          .toList();
+    });
     return Future.value(imageFutures);
   }
 }
 
 class ImageRepositoryFlameLink implements ImageRepositoryInterface {
-
   final FlameLink _cms;
 
   ImageRepositoryFlameLink(FlameLink cms) : _cms = cms;
 
   @override
   Future<ImageEntity> get(String uri) {
-     if (uri.isEmpty) {
+    if (uri.isEmpty) {
       return null;
     }
     final baseCollection = _cms.files();
@@ -109,7 +116,8 @@ class ImageRepositoryFlameLink implements ImageRepositoryInterface {
 
   @override
   Future<List<ImageEntity>> getImages(List<String> paths) {
-    final collection = FlamelinkDocumentCollection.list(cms: _cms, paths: paths);
+    final collection =
+        FlamelinkDocumentCollection.list(cms: _cms, paths: paths);
     return ImageEntityCollectionFlamelink(collection: collection).getEntities();
   }
 
@@ -122,7 +130,7 @@ class ImageRepositoryFlameLink implements ImageRepositoryInterface {
     final _typeTransform =
         StreamTransformer<DocumentSnapshot, ImageEntity>.fromHandlers(
             handleData: (snapshot, sink) {
-               sink.add(_transform(_cms, snapshot));
+      sink.add(_transform(_cms, snapshot));
     });
     return baseCollection.document(uri).snapshots().transform(_typeTransform);
   }
@@ -132,5 +140,4 @@ class ImageRepositoryFlameLink implements ImageRepositoryInterface {
     final storageReference = _cms.images(path: image.path);
     return storageReference.getDownloadURL();
   }
-
 }
