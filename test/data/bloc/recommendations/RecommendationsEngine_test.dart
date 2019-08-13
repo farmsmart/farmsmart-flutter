ba8e30ce7a61f69be7fa9e600ec06e8713ea25a1
Lee Higgins <lee@wearemobilefirst.com>
2019-08-13 13:17:56 +0200

    Use correct test input

--- test/data/bloc/recommendations/RecommendationsEngine_test.dart
+++ test/data/bloc/recommendations/RecommendationsEngine_test.dart
@@ -3,6 +3,11 @@ import 'package:farmsmart_flutter/model/model/mock/MockRecommendation.dart';
 import 'package:test/test.dart';
 
 final _inputScale = 10.0;
+final _plotInput = {
+  "Skill Level": "hi",
+  "Location": "lo",
+  "Agrozone": "med",
+};
 
 void main() {
   test('Harry Input recommendation', () {
@@ -12,7 +17,7 @@ void main() {
       weightMatrix: harryWeights,
     );
 
-    final chillies = chilliesEngine.recommend("Chillies", plotInfo);
+    final chillies = chilliesEngine.recommend("Chillies", _plotInput);
     final clampedToUnit = (chillies >= 0) && (chillies <= 1.0);
     final highlyRecommended = (chillies >= 0.8); 
     expect(clampedToUnit, true);
